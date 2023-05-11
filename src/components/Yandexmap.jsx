2307a6f82d24141cb9d3987b3d5eded058874189
Perevyazko <a.perevyazko@gmail.com>
2023-05-11 12:49:04 +0300

    setting load map

--- src/components/Yandexmap.jsx
+++ src/components/Yandexmap.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import { YMaps, Map } from "@pbe/react-yandex-maps";
+import {YMaps, Map} from "@pbe/react-yandex-maps";
 import {usePosition} from "../usePosition";
 
 
-const Yandexmap = () => {
+const YandexMap = () => {
         const { latitude, longitude, error } = usePosition();
-        console.log(latitude)
+    if (!latitude) {
+    return <div>Loading...</div>;
+  }
+
     return (
   <YMaps>
     <div className={"map"}>
         {error}
         <h2>Your geolocation</h2>
-      <Map width="100%" defaultState={{ center: [latitude,longitude], zoom: 12 }} />
+      <Map defaultState={{ center: [latitude, longitude], zoom: 15 }} width="100%" height="400px" />
     </div>
   </YMaps>
     );
 };
 
-export default Yandexmap;
+export default YandexMap;
 
